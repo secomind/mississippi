4b2eba7a66ccbf18dce860a1ac045fefff27fc18
Arnaldo Cesco <arnaldo.cesco@secomind.com>
2024-05-09 11:39:44 +0200

    Remove boilerplate hello world
    Signed-off-by: Arnaldo Cesco <arnaldo.cesco@secomind.com>

--- lib/mississippi.ex
+++ lib/mississippi.ex
@@ -2,17 +2,4 @@ defmodule Mississippi do
   @moduledoc """
   Documentation for `Mississippi`.
   """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Mississippi.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
--- test/mississippi_test.exs
+++ test/mississippi_test.exs
@@ -1,8 +1,4 @@
 defmodule MississippiTest do
   use ExUnit.Case
   doctest Mississippi
-
-  test "greets the world" do
-    assert Mississippi.hello() == :world
-  end
 end
