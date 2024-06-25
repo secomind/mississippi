e974a481de61ec2d255e0525c85df115483ef1ed
Arnaldo Cesco <arnaldo.cesco@secomind.com>
2024-06-25 17:06:11 +0200

    Consumer: define a cleaner supervision structure
    Now, each DataUpdater, MessageTracker, DataConsumer is supervised.
    DataConsumers are statically supervised, as we expect queues to be
    alwas there, while MessageTrackers and DataUpdaters can
    start and terminate dynamically.
    
    Signed-off-by: Arnaldo Cesco <arnaldo.cesco@secomind.com>

---
+++ lib/consumer/data_updater_supervisor.ex
@@ -0,0 +1,32 @@
+defmodule Mississippi.Consumer.DataUpdater.Supervisor do
+  require Logger
+  use DynamicSupervisor
+
+  def start_link(init_args) do
+    DynamicSupervisor.start_link(__MODULE__, init_args, name: __MODULE__)
+  end
+
+  @impl true
+  def init(init_args) do
+    _ = Logger.info("Starting DataUpdater supervisor", tag: "data_updater_supervisor_start")
+    DynamicSupervisor.init(strategy: :one_for_one, extra_arguments: init_args)
+  end
+
+  def start_child(child) do
+    _ =
+      Logger.info("Adding a new DataUpdater",
+        tag: "data_updater_add"
+      )
+
+    DynamicSupervisor.start_child(__MODULE__, child)
+  end
+
+  def terminate_child(pid) do
+    _ =
+      Logger.info("Terminating a DataUpdater",
+        tag: "data_updater_terminate"
+      )
+
+    DynamicSupervisor.terminate_child(__MODULE__, pid)
+  end
+end
