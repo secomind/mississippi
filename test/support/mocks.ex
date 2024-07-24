f3fec047778c28b17893ee3cf484f3799903ae77
Francesco Noacco <francesco.noacco@secomind.com>
2024-07-24 15:16:58 +0200

    refactor(amqp_connection): move to top-level
    this way we can implement eventproducer's own amqp connection
    
    Signed-off-by: Francesco Noacco <francesco.noacco@secomind.com>

--- test/support/mocks.ex
+++ test/support/mocks.ex
@@ -1,2 +1,2 @@
 Hammox.defmock(MockMessageHandler, for: Mississippi.Consumer.DataUpdater.Handler)
-Hammox.defmock(MockAMQPConnection, for: Mississippi.Consumer.AMQPDataConsumer.AMQPConnection)
+Hammox.defmock(MockAMQPConnection, for: Mississippi.AMQPConnection)
