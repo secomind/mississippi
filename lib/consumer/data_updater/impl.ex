edc5db2d62a541604fd8157049d4a36c4abcc865
Arnaldo Cesco <arnaldo.cesco@secomind.com>
2024-08-13 16:52:23 +0200

    Consumer: refine the Handler behaviour on received messages
    Allow more flexibility when handling messages. The `handle_message/5`
    callback now returns threeither ack (former ok), discard (former error) or stop.
    This way, a user can also request termination of a DU/MT pair when handling
    a message.
    Add also the possibility of computation after a message has been acked/discarded
    by introducing the `handle_continue/2` callback, which works pretty much like
    a GenServer's.
    
    Edit the default Handler implementation accordingly.
    
    Signed-off-by: Arnaldo Cesco <arnaldo.cesco@secomind.com>

--- lib/consumer/data_updater/impl.ex
+++ lib/consumer/data_updater/impl.ex
@@ -14,13 +14,19 @@ defmodule Mississippi.Consumer.DataUpdater.Handler.Impl do
       "Received message #{inspect(message_id)} with payload #{inspect(payload)} and headers #{inspect(headers)} at #{inspect(DateTime.from_unix!(timestamp))}"
     )
 
-    {:ok, :ok, state}
+    {:ack, :ok, state}
   end
 
   @impl true
   def handle_signal(signal, state) do
     IO.puts("Received signal #{inspect(signal)}")
 
+    {:ok, state}
+  end
+
+  @impl true
+  def handle_continue(continue_arg, state) do
+    IO.puts("Received continue with arg #{inspect(continue_arg)}")
     {:ok, state}
   end
 
